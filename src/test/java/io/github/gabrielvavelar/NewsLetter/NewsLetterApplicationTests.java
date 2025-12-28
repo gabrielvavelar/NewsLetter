131b9af78f7adea82d74f6139e165d1f8669a02e
GabrielAvelarbr <99gabrielavelar@gmail.com>
2025-12-28 18:03:54 -0300

    Initial commit

---
+++ src/test/java/io/github/gabrielvavelar/NewsLetter/NewsLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielvavelar.NewsLetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsLetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
